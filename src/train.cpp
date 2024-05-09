39d3d25c77a4ac66bc36b4a8e8351253a8f81eab
ShuvalovaEA <eshalos1207@gmail.com>
2024-05-09 17:43:35 +0300

    B222

--- src/train.cpp
+++ src/train.cpp
@@ -8,27 +8,37 @@ Train::Train() {
 
 void Train::addCage(bool light) {
     Cage* node = new Cage;
-    node->light = light;
-    if (first == nullptr) {
+    if (!first) {
+        node->light = light;
+        node->prev = node;
+        node->next = node;
         first = node;
-        first->prev = node;
-        first->next = node;
     } else {
-        node->prev = first;
-        node->next = first->next;
-        first->next->prev = node;
-        first->next = node;
+        Cage* SecFirst = first;
+        node->light = light;
+        node->next = first;
+        node->prev = first->prev;
+        first->prev->next = node;
+        first->prev = node;
     }
 }
 
 int Train::getLength() {
-    Cage* current = first;
-    int length = 0;
-    while (current != nullptr) {
-        current = current->next;
-        length++;
+    first->light = true;
+    Cage *SecFirst = first;
+    int q = 1;
+    int carriages = 1;
+    while (first->light) {
+        carriages = 1;
+        SecFirst = first->next;
+        while (!SecFirst->light) {
+            SecFirst = SecFirst->next;
+            carriages += 1;
+        }
+        SecFirst->light = false;
+        countOp += carriages * 2;
     }
-    return length;
+    return carriages;
 }
 
 int Train::getOpCount() {
