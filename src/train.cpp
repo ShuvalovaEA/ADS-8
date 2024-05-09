bacc8261ddcc1a1f022c7ab7b59976d2ead0b8de
ShuvalovaEA <eshalos1207@gmail.com>
2024-05-09 16:48:06 +0300

    add1

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,29 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() {
+    first = new Cage;
+    first->prev = nullptr;
+    first->light = false;
+    first->next = first;
+    countOp = 0;
+    length = 0;
+}
+
+void Train::addCage(bool light) {
+    Cage* temp = first;
+    first = new Cage;
+    first->prev = temp;
+    first->next = temp->next;
+    first->next->prev = first;
+    first->light = light;
+    length++;
+}
+
+int Train::getLength() {
+    return length;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
